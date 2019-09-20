bdf00b7b2822b264e1e3214ea3f89e6068e09eef
Leon Dail <leondailofficial@gmail.com>
2019-09-20 16:42:08 -0700

    added loading icon

--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import LoadingLogo from './150.gif'
+import './Loading.css'
 
 export default function Loading() {
   return(
-  <div>
-    <img src="./153.gif" alt="loading" />
+  <div class="loading">
+    <img src={LoadingLogo} alt="loading" />
   </div>
   );
 }
